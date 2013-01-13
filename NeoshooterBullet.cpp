aa3e4fa10e1ef0b6b2e8eb3366f8261e5840f5ad
Marlon Etheredge <marlon.etheredge@gmail.com>
2013-01-13 17:00:57 +0100

    Adding bullets, physics for bullets, neoshooter weapon, fixes for some stuff, activator markers (indicators) and some additional fixes

---
+++ NeoshooterBullet.cpp
@@ -0,0 +1,87 @@
+#include "StdAfx.h"
+
+#include "NeoshooterBullet.h"
+
+NeoshooterBullet::NeoshooterBullet(Player *owner) : Bullet(owner)
+{
+    this->m_Speed = 2.0f;
+
+    // Set the direction vector to the current direction of the player
+    this->m_DirectionVector = owner->GetDirectionVector();
+
+    // Initialize the bullet
+    this->Initialize();
+}
+
+NeoshooterBullet::~NeoshooterBullet(void)
+{
+    // Delete the sprite
+    delete this->m_Sprite;
+
+    // Delete physics stuff
+    cpSpace *space = this->m_Engine->GetWorld()->GetSpace();
+    cpSpaceRemoveShape(space, this->m_Shape);
+    cpSpaceRemoveBody(space, this->m_Body);
+    cpShapeFree(this->m_Shape);
+    cpBodyFree(this->m_Body);
+}
+
+void NeoshooterBullet::Initialize(void)
+{
+    // Load the texture
+    if (!(this->m_Texture = this->m_Hge->Texture_Load("data/particles.png")))
+	{
+		MessageBox(NULL, "Can't load particles.png texture.", "Error", MB_OK | MB_ICONERROR | MB_APPLMODAL);
+		this->m_Engine->Shutdown();
+		
+        return;
+	}
+
+    // Get the color of the owner
+    DWORD color = this->m_Owner->GetColor();
+
+    // Create the player sprite
+    this->m_Sprite = new hgeSprite(this->m_Texture, 40, 40, 20, 20);
+    this->m_Sprite->SetColor(color);
+	this->m_Sprite->SetHotSpot(10, 10);
+
+    // Construct the physics stuff
+    {
+        // Define moment
+        cpFloat moment = cpMomentForCircle(1, 0, 10, cpvzero);
+
+        // Fetch the physics space
+        cpSpace *space = this->m_Engine->GetWorld()->GetSpace();
+
+        // Add the physics body
+        this->m_Body = cpSpaceAddBody(space, cpBodyNew(1, moment));
+        cpBodySetPos(this->m_Body, *this->m_Position);
+
+        // Create and add the poly shape
+        this->m_Shape = cpSpaceAddShape(space, cpCircleShapeNew(this->m_Body, 10, cpvzero));   
+        this->m_Shape->sensor         = true;
+        this->m_Shape->data           = this;
+        this->m_Shape->collision_type = COLLISION_TYPE_BULLET;
+    }
+    
+    // Intialize the base
+    this->InitializeBullet();
+}
+
+void NeoshooterBullet::Render(float dt)
+{
+    // Render the bullet
+    this->m_Sprite->Render(this->m_Position->x, this->m_Position->y);
+}
+
+void NeoshooterBullet::Update(float dt)
+{
+    // Fix the velocity
+    this->m_Body->v = cpvmult(this->m_Body->v, this->m_Speed / cpvdist(cpVect(), this->m_Body->v));
+
+    // Steer the bullet in the right direction
+    this->m_Body->v.x = this->m_DirectionVector.x * 2.2f;
+
+    // Update the position
+    this->m_Position = new cpVect(cpBodyGetPos(this->m_Body));
+}
---
+++ NeoshooterBullet.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Bullet.h"
+
+class NeoshooterBullet : public Bullet
+{
+private:
+    HTEXTURE m_Texture;
+
+    hgeSprite *m_Sprite;
+
+    cpShape *m_Shape;
+    cpBody  *m_Body;
+public:
+    NeoshooterBullet(Player *owner);
+    ~NeoshooterBullet(void);
+
+    void Initialize(void);
+    void Render(float dt);
+    void Update(float dt);
+};
